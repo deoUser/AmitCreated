c1f5ae7dd7756604c3b06bdd39b67360f7dc96ed
Amitgupta29 <amit.gupta@appistoki.com>
2016-11-07 17:49:14 +0530

    Commited Second Time

--- DabbaProject/src/sam/appistoki/am/Guru.java
+++ DabbaProject/src/sam/appistoki/am/Guru.java
@@ -4,7 +4,7 @@ public class Guru {
 	
 	public static void main(String[] args) {
 		System.out.println("This is my change");
-		System.out.println("hi i mis u");
+		System.out.println("hi i mis u tooooooooooooooooooooooooooo");
 	}
 
 }
